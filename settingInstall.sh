2a13f15c6c5507a69b3ae148db9922a5c72af9f1
murajo <zorinmurajo@gmail.com>
2018-08-17 22:42:37 +0900

    パスを修正

--- settingInstall.sh
+++ settingInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/murajo/vim_plugin ~/dotfiles
-git clone https://github.com/Shougo/neobundle.vim ~/vimfiles/bundle/neobundle.vim
+git clone https://github.com/Shougo/neobundle.vim ~/dotfiles/vimfiles/bundle/neobundle.vim
 ln -Fis ~/dotfiles/.vimrc ~/.vimrc
 ln -Fis ~/dotfiles/vimfiles ~/.vim
 vim ~/dotfiles/.vimrc -c NeoBundleInstall
